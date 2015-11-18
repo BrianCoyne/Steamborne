1e9e13ff5c4255df7df4bb0873a77b40f2d8fc28
BrianCoyne <brian.coyne@pulsecollegestudent.com>
2015-11-18 13:08:08 +0000

    Brian 11/18
    GUI + Test

---
+++ SteamBornPrototype/Assets/Scripts/ResourceText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResourceText : MonoBehaviour {
+
+	public Text textresourceobject;
+	
+	// Use this for initialization
+	void Start () {
+		textresourceobject = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		textresourceobject.text = "" + BuildingPlacement.Instance.CurrentPopulation + "/" + BuildingPlacement.Instance.CurrentHousing + "     " + BuildingPlacement.Instance.CurrentRivets + "     " + BuildingPlacement.Instance.CurrentVolts;
+	}
+}	
