e05cbf36f9124f0e0119dfde4b62006df709deba
Eoin <eoinbt@gmail.com>
2015-11-25 15:56:20 +0000

    eoin commit

---
+++ SteamBornPrototype/Assets/Scripts/ClickSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickSound : MonoBehaviour {
+
+	public AudioClip click;
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+
+	// Use this for initialization
+	void Update(){
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			Debug.Log ("Click");
+			audio.PlayOneShot (click, 1.0f);
+		}
+	}
+}
